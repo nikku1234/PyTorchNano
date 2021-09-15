83901560c58955bc12c4fe71941c383d297127dd
nikku1234 <nikhilramesh1234@gmail.com>
2021-09-14 21:01:26 -0400

    Added the skeleton code for all the files

---
+++ Assignment 1/GraphNet.py
@@ -0,0 +1,10 @@
+class GraphNet:
+    def __init__(self,graphnodes) -> None:
+        self.graphnodes = graphnodes
+        # pass
+    
+    def forward(self):
+        return None
+
+    def backward(self):
+        return None
---
+++ Assignment 1/Utils.py
@@ -0,0 +1,1 @@
+# For defining any helper functions
