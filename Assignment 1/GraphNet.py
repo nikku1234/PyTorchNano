d92f422510cefa2f34889afcbf9fbb0eb2c962ab
nikku1234 <nikhilramesh1234@gmail.com>
2021-09-21 13:10:43 -0400

    Updated code

--- Assignment 1/GraphNet.py
+++ Assignment 1/GraphNet.py
@@ -1,10 +1,15 @@
 class GraphNet:
     def __init__(self,graphnodes) -> None:
         self.graphnodes = graphnodes
+        self.order = []
         # pass
     
-    def forward(self):
-        return None
+    # def forward(self):
+    #     return None
 
-    def backward(self):
-        return None
+    # def backward(self):
+    #     return None
+
+    def add(self,layer):
+        self.order.append(layer)
+
--- Assignment 1/Train.py
+++ Assignment 1/Train.py
@@ -1,14 +1,10 @@
-from Model import create_model
+# from Model import create_model
+import numpy as np
+# from mnist import MNIST
 
-
-
-
-
-
-
-
-
-
+# mnist = MNIST('../dataset/MNIST')
+# x_train, y_train = mnist.load_training()  # 60000 samples
+# x_test, y_test = mnist.load_testing()  # 10000 samples
 
 
 
@@ -21,3 +17,7 @@ from Model import create_model
 # The following method would create the model 
 #---------------------------------------------
 model = create_model()
+# x_train = np.asarray(x_train).astype(np.float32)
+# y_train = np.asarray(y_train).astype(np.int32)
+# x_test = np.asarray(x_test).astype(np.float32)
+# y_test = np.asarray(y_test).astype(np.int32)
