778da451e94e564842b5481e53eae22fc934e101
nikku1234 <nikhilramesh1234@gmail.com>
2021-09-26 22:49:49 -0400

    uneven check

--- convolution_forward_sample.py
+++ convolution_forward_sample.py
@@ -140,9 +140,13 @@ new_kernal = np.array([[1, 0, 1],
                        [0, 1, 0],
                        [-1, 1, 0]]).reshape(3, 3)
 
+# new_image = np.random.randint(3, size=(25, 25))
+
+# new_kernal = np.random.randint(3, size=(7, 3))
+
 padding = 1
 
-
+#numpy.org
 def pad_with(vector, pad_width, iaxis, kwargs):
     pad_value = kwargs.get('padder', 0)
     vector[:pad_width[0]] = pad_value
