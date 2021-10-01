d57444333ad752ccebf9ef4aadd84c81a7594970
Amrit Sreekumar <amrit.sreekumar@gmail.com>
2021-09-30 21:39:06 -0400

    Completed

--- Assignment_1/testing.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-x = [1,0,-2]
-for i in range(len(x)):
-    if x[i]>0:
-        x[i]=1
-    else:
-        x[i]= 0
-print(x)
--- broadcast.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-img = np.random.random_integers(0, 255, (5, 5, 5))
-kernal = np.random.random_integers(0, 255,(10,5,5, 5))
-# in_channels = 5
-# out_channels = 10
-# new_kernal = np.tile(kernal, out_channels)
-print((img*kernal).shape)
